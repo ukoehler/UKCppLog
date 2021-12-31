29613ce209ce9e893b6ae208e58b1f9a42595372
Dr. Uwe Köhler <13613987+ukoehler@users.noreply.github.com>
2021-12-31 16:26:13 +0100

    Add Utils.cmake
    Improved version of UKLogger.cpp
    
    Add UKLoggerLibTest.cpp. Aim is for this to call the other test executables and check the output.
    Also, it ensures all of them are called for code coverage.

--- src/UKLogger.cpp
+++ src/UKLogger.cpp
@@ -1,25 +1,26 @@
 /*
- * <Basic C++ logger library.>
- * Copyright 2020  Dr. Uwe Köhler <U.Koehler@gmx.de>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <Basic C++ logger library.>
+* Copyright 2020  Dr. Uwe Köhler <U.Koehler@gmx.de>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License or (at your option) version 3 or any later version
+* accepted by the membership of KDE e.V. (or its successor approved
+* by the membership of KDE e.V.), which shall act as a proxy
+* defined in Section 14 of version 3 of the license.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "UKLogger.hpp"
+#include <algorithm>
 #include <chrono>
 #include <cstdio>   // remove
 #include <cstdlib>  // exit, EXIT_FAILURE
@@ -43,7 +44,7 @@ UKLogger::UKLogger() : mInitialBuffer(""), mFileStream(nullptr) {
 UKLogger::~UKLogger() { moveToTerminal(); }
 
 void UKLogger::log(const std::string& severity, const std::string& kind, const std::string& func,
-                   const std::string& message, int line) {
+                const std::string& message, int line) {
     // get current time
     auto now = std::chrono::system_clock::now();
 
@@ -69,6 +70,14 @@ void UKLogger::log(const std::string& severity, const std::string& kind, const s
     if (function.size() - 6 == function.rfind(" const")) {
         function.erase(function.size() - 6, 6);
     }
+    // remove leading virtual
+    if (0 == function.find("virtual ")) {
+        function.erase(0, 8);
+    }
+    // remove leading static
+    if (0 == function.find("static ")) {
+        function.erase(0, 7);
+    }
     // remove function return declaration for non-constructors
     size_t openingBraket = function.find('(');
     size_t space         = function.find(' ');
@@ -82,15 +91,43 @@ void UKLogger::log(const std::string& severity, const std::string& kind, const s
         function.erase(openingBraket + 1, closeingBraket - openingBraket - 1);
         function.insert(openingBraket + 1, "...");
     }
+    // handle namespaces in function
+    if (47 < function.size()) {
+        unsigned int numberOfDoubleColons = 0;
+        size_t nPos = 0;
+        size_t lastDoubleColon = 0;
+        size_t secondLastDoubleColon = 0;
+        while ((nPos = function.find("::", nPos)) != std::string::npos) {
+            numberOfDoubleColons++;
+            secondLastDoubleColon = lastDoubleColon;
+            lastDoubleColon = nPos;
+            nPos += 2;
+        }
+        if (47 > (function.size()-secondLastDoubleColon+1)) {
+            function.erase(0, secondLastDoubleColon + 2);
+            function = "..." + function;
+        } else {
+            if (47 > (function.size()-lastDoubleColon+1)) {
+                function.erase(0, lastDoubleColon + 2);
+                function = "..." + function;
+            }
+        }
+//         size_t doubleColon = function.find("::");
+//         while (doubleColon < lastDoubleColon) {
+//             function.erase(0, doubleColon + 2);
+//             doubleColon = function.find("::");
+//         }
+    }
     s.clear();
     s.str("");
     s << std::setfill(' ') << timeStr << " " << std::left << std::setw(8) << severity << " "
-      << "[" << std::right << std::setw(6) << threadID << "] "
-      << "(" << std::left << std::setw(12) << kind << ") " << std::right << std::setw(55) << function << " "
-      << std::right << std::setw(6) << line << ": " << message << std::endl;
+    << "[" << std::right << std::setw(6) << threadID << "] "
+    << "(" << std::left << std::setw(20) << kind.substr(0, 20) << ") "
+    << std::right << std::setw(47) << function.substr(0, 47) << " "
+    << std::right << std::setw(6) << line << ": " << message << std::endl;
     std::lock_guard<std::mutex> lockGuard(mMutex);
     if (mFileOpen) {
-        mFileStream << s.str() << std::endl << std::flush;
+        mFileStream << s.str() << std::flush;
     } else {
         mInitialBuffer += s.str();
     }
